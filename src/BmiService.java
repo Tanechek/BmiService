699588b3798dee02ad179b69b41af6812ab9c814
Tatyana.Popovskaya <ssttaa87@yandex.ru>
2022-06-22 23:19:58 +0700

    Formula fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,7 @@
 public class BmiService {
 
     public double calculate(double m, double h) {
-        double Bmi;
-        {
-            Bmi = m / (h * h);
-        }
+        double Bmi = m / (h * h);
         return Bmi;
     }
 }
